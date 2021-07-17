138d2f81573e1d6cf8c7248c5fff8b177783d441
vighneshkadam007 <50268642+vighneshkadam007@users.noreply.github.com>
2021-07-17 22:48:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express=require("express");
+require("dotenv").config()
+const app=express();
+const fetch=require("node-fetch")
+app.set('view engine','ejs')
+app.listen(3000);
+
+app.get('/',async (req,res)=>{
+   //res.send('<p>ererere</p>')
+   const url=`https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${process.env.API_KEY}`;
+   const data=await fetch(url);
+   const news =await data.json();
+   const arr=[];
+   const a2=[];
+   const t=[]
+    news.results.map(pieces=>{
+  //    console.log(process.env.API_KEY)
+      console.log(pieces.title)
+      arr.push(pieces.title)
+      t.push(pieces.url)
+      a2.push(pieces.multimedia[1].url)
+    })
+    res.render("home",{arr,a2,t})
+    console.log("hello")
+  //console.log(news)
+  });
+  
